a3459aa5b7bb7ae1a4390e6bfd0e61b09e4fc86e
Волчков Станислав <volch.ok2420@gmail.com>
2022-05-27 18:42:27 +0300

    reggreg

--- lib/model.py
+++ lib/model.py
@@ -2,9 +2,9 @@ from typing import Tuple, Union
 import pandas as pd
 import numpy as np
 
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
+#import nltk
+#from nltk.corpus import stopwords
+#from nltk.tokenize import word_tokenize
 import re
 import pymorphy2
 
